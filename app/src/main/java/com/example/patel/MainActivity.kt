d8e96d4a4c7cb58f77ce2f9e0d9ce69792220755
vrajrpatel <patel.vraj1781@gmail.com>
2022-12-01 11:04:51 -0800

    Capstone

---
+++ app/src/main/java/com/example/patel/MainActivity.kt
@@ -0,0 +1,18 @@
+ package com.example.patel
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    private val answers = listOf("Sure!", "Not today", "Ask a friend", "You decide", "Are you serious?", "Possible", "Uncertain")
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+
+        //user clicks on button
+
+
+        }
+    }

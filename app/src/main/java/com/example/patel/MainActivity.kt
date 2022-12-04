21cf73531a0cbf9b3ede4705563025022d2a79ae
fquach <Fifiq28@gmail.com>
2022-12-03 17:14:00 -0800

    added classic 8ball responses to answers list

--- app/src/main/java/com/example/patel/MainActivity.kt
+++ app/src/main/java/com/example/patel/MainActivity.kt
@@ -5,14 +5,35 @@ import android.os.Bundle
 
 class MainActivity : AppCompatActivity() {
 
-    private val answers = listOf("Sure!", "Not today", "Ask a friend", "You decide", "Are you serious?", "Possible", "Uncertain")
+    private val answers = listOf(
+            "It is certain.",
+            "It is decidedly so.",
+            "Without a doubt.",
+            "Yes definitely.",
+            "You may rely on it.",
+            "As I see it, yes.",
+            "Most likely.",
+            "Outlook good.",
+            "Yes.",
+            "Signs point to yes.",
+            "Reply hazy, try again.",
+            "Ask again later.",
+            "Better not tell you now.",
+            "Cannot predict now.",
+            "Concentrate and ask again.",
+            "Don't count on it.",
+            "My reply is no.",
+            "My sources say no.",
+            "Outlook not so good.",
+            "Very doubtful."
+    )
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
 
         //user clicks on button
-
+        //replace text view with new answer
 
         }
     }
